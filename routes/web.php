77ed6a835b2c6a3bc4db96cec50373489c942e99
Aibek Baldykov <baldykov_a@yahoo.com>
2019-03-14 21:17:03 +0600

    Add report route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $reports = \App\Report::all();
+
+    return view('welcome', compact('reports'));
+})->name('home');
+
+Route::get('/reports/create', 'Web\ReportController@create')->name('reports.create');
+Route::post('/reports/', 'Web\ReportController@store')->name('reports.store');
+Route::patch('/reports/{report}', 'Web\ReportController@update')->name('reports.update');
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+//    'middleware' => 'admin',
+    'as' => 'admin.',
+], function() {
+    Route::resource('reports', 'ReportController');
 });
